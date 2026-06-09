7bbc278aae6027322137ab07a731e40f4623693d
Kenisha <kenisha.koul@gmail.com>
2026-06-10 02:37:01 +0530

    fix API baseURL to backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000"
+  baseURL: "https://studybuddy-backend-production-f984.up.railway.app/"
 })
 
 export default API
