d69ea57f412f8b278bc3f0f0ec0fe8a3515f744c
Kenisha <kenisha.koul@gmail.com>
2026-06-10 10:31:17 +0530

    remove trailing slash from baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "https://studybuddy-backend-production-f984.up.railway.app/"
+  baseURL: "https://studybuddy-backend-production-f984.up.railway.app"
 })
 
 export default API
